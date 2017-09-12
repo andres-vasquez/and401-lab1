f3470da08a4282c87288745f542fc85a5fff1b01
andres-vasquez <andres.vasquez.a@hotmail.com>
2017-09-11 20:24:59 -0400

    Activity Lifecycle

--- app/src/main/java/com/example/android_user01/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android_user01/myapplication/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.android_user01.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String LOG = "CursoAndroid";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG,"onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG,"onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG,"onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG,"onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG,"onStop");
     }
 }
